08f3a3a7140125431e9a5056ccf45b6397956c98
Bjwills7 <bjwills7@hotmail.com>
2022-07-21 01:55:52 -0500

    Add button variables Will probably refactor into a loop

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-let test = 'success';
+const display = document.querySelector('.display');
+const percent = document.querySelector('.percent');
+const clear = document.querySelector('.clear');
+const square = document.querySelector('.square');
+const back = document.querySelector('.back');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const multiplyButton = document.querySelector('.multiply');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const subtractButton = document.querySelector('.subtract');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const add = document.querySelector('.add');
+const sign = document.querySelector('.sign');
+const zero = document.querySelector('.zero');
+const decimalButton = document.querySelector('.decimal');
+const computeButton = document.querySelector('.compute');
+
+function updateDisplay(button) {
+    
+}
